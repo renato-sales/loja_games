525e4ee5abae8cf88b1c39a60261738393155ba4
Renato Sales <renatosalespaula@gmail.com>
2026-01-19 11:18:27 -0300

    implementação da entidade categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  nome: string;
+}
