145c0e48524242b651ca151ac1d19aab40f06ddd
Renato Sales <renatosalespaula@gmail.com>
2026-01-19 13:37:17 -0300

    implementação do controller da categoria

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaController } from './controller/categoria.controller';
 import { Categoria } from './entities/categoria.entity';
 import { CategoriaService } from './services/categoria.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Categoria])],
   providers: [CategoriaService],
-  controllers: [],
+  controllers: [CategoriaController],
   exports: [],
 })
 export class CategoriaModule {}
---
+++ src/categoria/controller/categoria.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Categoria } from '../entities/categoria.entity';
+import { CategoriaService } from '../services/categoria.service';
+
+@Controller('categorias')
+export class CategoriaController {
+  constructor(private readonly categoriaService: CategoriaService) {}
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
+    return this.categoriaService.findById(id);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaService.findAll();
+  }
+
+  @Get('/nome/:nome')
+  @HttpCode(HttpStatus.OK)
+  findAllByNome(@Param('nome') nome: string): Promise<Categoria[]> {
+    return this.categoriaService.findAllByNome(nome);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.create(categoria);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.update(categoria);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriaService.delete(id);
+  }
+}
